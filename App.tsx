7e5e6bcaa31608acc77ef5c32b06553c066c0e45
Sam <samarony.barros@gmail.com>
2020-02-22 17:12:41 +0100

    Removes unecessary libraries

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import Button from './src/components/Button'
 import Display from './src/components/Display'
 
